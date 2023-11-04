50b2d29a3a20e103c4d3d4b5601ad40ade0e74e4
Bhaskarpratik1234 <144020186+Bhaskarpratik1234@users.noreply.github.com>
2023-11-04 18:36:33 +0530

    updated one

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,1 +1,17 @@
-(document.querySelectorAll("[toast-list]")||document.querySelectorAll("[data-choices]")||document.querySelectorAll("[data-provider]"))&&(document.writeln("<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/toastify-js'><\/script>"),document.writeln("<script type='text/javascript' src='assets/libs/choices.js/public/assets/scripts/choices.min.js'><\/script>"),document.writeln("<script type='text/javascript' src='assets/libs/flatpickr/flatpickr.min.js'><\/script>"));
+(document.querySelectorAll("[toast-list]") ||
+  document.querySelectorAll("[data-choices]") ||
+  document.querySelectorAll("[data-provider]")) &&
+  (document.writeln(
+    "<script type='text/javascript' src='https://cdn.lordicon.com/gsqxdxog.json'></script>"
+  ),
+  document.writeln(
+    "<script type='text/javascript' src='assets/libs/choices.js/public/assets/scripts/choices.min.js'></script>"
+  ),
+  document.writeln(
+    "<script type='text/javascript' src='assets/libs/clockpicker/bootstrap-clockpicker.min.js'></script>"
+  ),
+  document.writeln(
+    "<script type='text/javascript' src='assets/libs/flatpickr/flatpickr.min.js'></script>"
+  ));
+
+
